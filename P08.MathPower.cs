aa101435e1415e20be77834ae8ee8bac1d8011b5
vpelov <vpelov@mail.bg>
2022-02-04 13:07:59 +0200

    Add files via upload

---
+++ P08.MathPower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetPower(numberOne, numberTwo));
+        }
+
+        private static int GetPower(int numFirst, int numSekond) 
+        {
+            int totalSum = 1;
+            for (int i = 0; i < numSekond; i++)
+            {
+                totalSum *= numFirst;
+            }
+            return totalSum;
+        }
+    }
+}
